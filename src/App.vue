1c2679da2e373b2b0f2a123d8744c1081302abe8
Administrator <576303283@qq.com>
2021-02-16 13:59:15 +0800

    ffff

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>fs</div>
+    <div>fsdd</div>
     <Header @adds="add" />
     <div v-for="(item, idx) in list" :key="idx" style="display: flex">
       <div style="margin-right: 10px">{{ item }}</div>
